8a8ba054f7de346b24324fc5c82804366a537049
RiverBreaker <lyq203q@163.com>
2026-01-01 00:37:27 +0800

    test

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # build-mingw64-toolchain-test
+
+${PREFIX} : gcc binutils gdb mingw64-tools mingw-w64-libraries-libmangle make cmake
+
+${PREFIX}/${TARGET} mingw-w64-libraries-winpthreads mingw-w64-headers mingw-w64-crt libiconv
+
+${PREFIX}/opt zlib openssl sqlite python expat xz bzip2 gdbm readline ncurses libffi libgnurx
--- scripts/build-component/download-source.sh
+++ scripts/build-component/download-source.sh
@@ -1,14 +1,16 @@
-#! /usr/bin/env sh
 set -euo pipefail
+export GITHUB_WORKSPACE="/home/build/build-mingw"
+export GCC_VERSION="13.2.0"
 source "${GITHUB_WORKSPACE}/scripts/functions.sh"
+
 info "GCC 版本: ${GCC_VERSION}"
-run "获取组件版本信息" export_versions_from_json "${DEP_ISON_FILE}" "${GCC_VERSION}" local
+DEP_JSON_FILE="${GITHUB_WORKSPACE}/dependencies.json"
+run "获取组件版本信息" export_versions_from_json "${DEP_JSON_FILE}" "${GCC_VERSION}" local
 
 export GITHUB_BASE_URL="https://github.com"
 export GNU_BASE_URL="https://ftp.gnu.org/gnu"
 
 # optional tuning
-LOG_DIR="${GITHUB_WORKSPACE}/logs"
 export FORCE_COLOR=1
 mkdir -p "${LOG_DIR}"
 IGNORE_REGEX='array subscript 0 is outside array bounds'
@@ -17,7 +19,7 @@ FAIL_ON_FATAL_IN_SUCCESS=1
 
 export SOURCE_CODE_DIR="${GITHUB_WORKSPACE}/source-code" && mkdir -p "${SOURCE_CODE_DIR}"
 
-gnu_pkgs=( gcc binutils libiconv make ncurses readline gdb )
+gnu_pkgs=( gcc binutils libiconv make readline gdb )
 github_pkgs=( mingw-w64 ninja cmake pkgconf expat zlib )
 
 for p in "${gnu_pkgs[@]}";do
@@ -36,57 +38,61 @@ done
 for p in "${github_pkgs[@]}";do
   case "${p}" in
     mingw-w64)
-      ver="${MINGW_W64}"
-      github_author="mingw-w64"
-      project_name="mingw-w64"
-      ;;
-    python)
-      ver="${PYTHON}"
-      github_author="python"
-      project_name="cpython"
-      ;;
-    openssl|sqlite|libffi|pkgconf)
-      pkg_name=${p^^}
-      ver="${!pkg_name}"
-      github_author="${p}"
-      project_name="${p}"
-      ;;
+    ver="${MINGW_W64}"
+    github_author="mingw-w64"
+    project_name="mingw-w64"
+    ;;
+    pkgconf)
+    pkg_name=${p^^}
+    ver="${!pkg_name}"
+    github_author="${p}"
+    project_name="${p}"
+    ;;
     ninja)
-      ver="${NINJA}"
-      github_author="ninja-build"
-      project_name="ninja"
-      ;;
+    ver="${NINJA}"
+    github_author="ninja-build"
+    project_name="ninja"
+    ;;
     cmake)
-      ver="${CMAKE}"
-      github_author="Kitware"
-      project_name="CMake"
-      ;;
+    ver="${CMAKE}"
+    github_author="Kitware"
+    project_name="CMake"
+    ;;
     expat)
-      ver="${EXPAT}"
-      github_author="libexpat"
-      project_name="libexpat"
-      ;;
+    ver="${EXPAT}"
+    github_author="libexpat"
+    project_name="libexpat"
+    ;;
     zlib)
-      ver="${ZLIB}"
-      github_author="madler"
-      project_name="zlib"
-      ;;
+    ver="${ZLIB}"
+    github_author="madler"
+    project_name="zlib"
+    ;;
+    pdcurses)
+    ver="${PDCURSES}"
+    github_author="wmcbrine"
+    project_name="PDCurses"
+    ;;
     *)
-      error "未知的 GitHub 包: ${p}"
-      exit 1
-      ;;
+    error "未知的 GitHub 包: ${p}"
+    exit 1
+    ;;
   esac
   if [[ "${p}" == "pkgconf" ]] || [[ "${p}" == "expat" ]]; then
     tail="${p}-${ver}.tar.gz"
+  elif [[ "${p}" == "pdcurses" ]]; then
+    tail="${ver}.tar.gz"
   else
     tail="v${ver}.tar.gz"
   fi
   EXPAT_TAG="R_${ver//./_}"
   if [[ "${p}" == "expat" ]]; then
-    URL="${GITHUB_BASE_URL}/${github_author}/${project_name}/release/download/${EXPAT_TAG}/${tail}"
+    URL="${GITHUB_BASE_URL}/${github_author}/${project_name}/releases/download/${EXPAT_TAG}/${tail}"
   else
     URL="${GITHUB_BASE_URL}/${github_author}/${project_name}/archive/refs/tags/${tail}"
   fi
   run "下载${p}-${ver} 源码" curl_download "${URL}" "${SOURCE_CODE_DIR}"
   run "解压${p}-${ver} 源码" archive_extract "${p}" "${SOURCE_CODE_DIR}/${tail}" "${SOURCE_CODE_DIR}"
 done
+
+info "下载与解压步骤完成"
